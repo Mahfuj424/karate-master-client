84344daf0b95c38d35265bf3f2b722cbeed3ddc8
mahfuj <mahfujahmad424@gmail.com>
2023-06-09 18:50:57 +0600

    first commit

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+
+const Main = () => {
+     return (
+          <div>
+               <h1>this is main compo</h1>
+          </div>
+     );
+};
+
+export default Main;
---
+++ src/Routes/Router.jsx
@@ -0,0 +1,22 @@
+import {
+   createBrowserRouter,
+} from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home/Home";
+
+
+const Router = createBrowserRouter([
+   {
+      path: '/',
+      element: <Main />,
+      children: [
+         {
+            path: '/',
+            element: <Home />
+            
+         }
+      ]
+      
+}
+])
+export default Router;
