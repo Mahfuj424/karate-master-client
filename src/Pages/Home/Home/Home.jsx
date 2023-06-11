33df5daee44840d297963899312a55e7dce0aba6
mahfuj <mahfujahmad424@gmail.com>
2023-06-12 04:25:07 +0600

    testimonial section added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Testimonial from '../UserReview/Testimonial';
 
 const Home = () => {
      return (
           <div>
-               <Banner/>
+               <Banner />
+               <Testimonial/>
           </div>
      );
 };
